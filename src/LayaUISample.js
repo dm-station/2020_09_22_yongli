c61eaaca866c8113c32687d34d9f025a1494ffbf
litaiming <834777406@qq.com>
2020-09-25 18:24:40 +0800

    永利测试完毕

--- src/LayaUISample.js
+++ src/LayaUISample.js
@@ -67,14 +67,39 @@ function sf(type,obj,num,time) {
 	Tween.to(obj,json,time,Ease.linearIn,Handler.create(self, function(){sf(type,obj,num,time);}));
 }
 
+function butterfly(obj,sx,sy,y,alpha){
+	var _y=obj.y
+	Tween.to(obj,{scaleX:sx,scaleY:sy,y:_y+y,alpha:alpha},450,Ease.linearIn, Handler.create(self, function(){
+		Tween.to(obj,{scaleX:1,scaleY:1,y:_y,alpha:1},750,Ease.linearIn, Handler.create(self, function(){
+			butterfly(obj,sx,sy,y,alpha)
+		}));		
+	}));
+};
+
+function rotation(obj,rotate,time){
+	Tween.to(obj,{rotation:rotate},time,Ease.linearIn, Handler.create(self, function(){
+		Tween.to(obj,{rotation:-rotate},time*2,Ease.linearIn, Handler.create(self, function(){
+			Tween.to(obj,{rotation:0},time,Ease.linearIn, Handler.create(self, function(){
+				rotation(obj,rotate,time)
+			}));
+		}));
+	}));
+}
 //x缩放比例=舞台宽度/设计宽度
 var scaleRateW
 //y缩放比例=舞台高度/设计高度
 var scaleRateH
 //token
-var token = ''
+var token = getRequestParams('token')||Math.random();
 // 当前层级
 var zindex = 0
+// 跳转链接
+var jump=''
+
+var latitude='1'; // 纬度，浮点数，范围为90 ~ -90
+var longitude='1'; // 经度，浮点数，范围为180 ~ -180。
+var speed; // 速度，以米/每秒计
+var accuracy; // 位置精度
 
 function p1() {
 	p1UI.super(this);
@@ -84,6 +109,214 @@ function p1() {
 	scaleRateW = (Laya.stage.width / Laya.stage.designWidth);//x缩放比例=舞台宽度/设计宽度
 	scaleRateH = (Laya.stage.height / Laya.stage.designHeight);//y缩放比例=舞台高度/设计高度
 	self.y=Laya.stage.height/2-Laya.stage.designHeight/2
+
+	if(Laya.stage.height<1500){
+		self.top1.y=self.top2.y=-self.y
+		self.logo.y=21-self.y
+		self.bottom1.y=1481+self.y
+		self.bottom2.y=1435+self.y
+	}
+	if(Laya.stage.height<=1220){
+		self.gift.y-=50
+		self.p1_t3.y-=70
+	}
+	sf(1,self.top1,1.03,800)
+	sf(2,self.top2,1.03,800)
+	rotation(self.bottom1,-1,600)
+	rotation(self.bottom2,-2,600)
+
+	butterfly(self.butterfly,0.8,0.95,0,0.8)
+	rotation(self.leaves,-8,400)
+
+	// self.p1_t1.y=-200
+	// Tween.to(self.p1_t1,{y:235}, 600, Ease.backOut, null, 10)
+	// self.p1_t2.alpha=0
+	// Tween.to(self.p1_t2,{alpha:1}, 600, Ease.bounceOut, null, 10)
+
+	// var poss=[0,37,375,37,375]
+	// for (var i = 1; i < 5; i++) {
+	// 	self['prz'+i].x=Laya.stage.width
+	// 	Tween.to(self['prz'+i],{x:poss[i]}, 700, Ease.backOut, null, i*400+200)
+	// }
+
+	self.gift.scale(0,0)
+	self.p1_t3.alpha=0
+	Tween.to(self.gift,{scaleX:1,scaleY:1}, 400, Ease.linearIn, Handler.create(self, function(){
+		Tween.to(self.p1_t3,{alpha:1}, 600, Ease.linearIn)
+		sf(1,self.gift,1.1,400)
+	}), 300)
+
+	self.gift.on(Event.CLICK,this,function () {
+		_czc.push(['_trackEvent', '首页', 'btn', '邂逅惊喜'])
+		Laya.stage.removeChild(self)
+		Laya.stage.addChildAt(new p2(),0)
+	})
+}
+
+function p2() {
+	p2UI.super(this);
+	var self = this;
+	var Event = laya.events.Event;
+
+	scaleRateW = (Laya.stage.width / Laya.stage.designWidth);//x缩放比例=舞台宽度/设计宽度
+	scaleRateH = (Laya.stage.height / Laya.stage.designHeight);//y缩放比例=舞台高度/设计高度
+	self.y=Laya.stage.height/2-Laya.stage.designHeight/2
+
+	if(Laya.stage.height<1500){
+		self.top1.y=self.top2.y=-self.y
+		self.logo.y=21-self.y
+		self.bottom1.y=1481+self.y
+		self.bottom2.y=1435+self.y
+	}
+	if(Laya.stage.height<=1220){
+		self.p2_btn.y-=40
+	}
+	sf(1,self.top1,1.03,800)
+	sf(2,self.top2,1.03,800)
+	rotation(self.bottom1,-1,600)
+	rotation(self.bottom2,-2,600)
+
+	butterfly(self.butterfly,0.8,0.95,0,0.8)
+	rotation(self.leaves,-8,400)
+
+	self.rule['_childs'][1].showButtons=false;
+	self.rule['_childs'][1].elasticBackTime = 200;//设置橡皮筋回弹时间。单位为毫秒。
+ 	self.rule['_childs'][1].elasticDistance = 50;//设置橡皮筋极限距离。
+
+	 var isCheck=false
+
+	 self.check.on(Event.CLICK,this,function () {
+		 if(self.check.skin==='p2/p2_check.png'){
+			_czc.push(['_trackEvent', '规则页', 'btn', '同意条款'])
+			 isCheck=true
+			 self.check.skin='p2/p2_checked.png'
+		 }else{
+			 _czc.push(['_trackEvent', '规则页', 'btn', '取消条款'])
+			 isCheck=false
+			 self.check.skin='p2/p2_check.png'
+		 }
+	})
+
+	 self.p2_btn.on(Event.CLICK,this,function () {
+		 if(isCheck){
+			 _czc.push(['_trackEvent', '规则页', 'btn', '轻启专属已同意'])
+			Laya.stage.removeChild(self)
+			Laya.stage.addChildAt(new p3(),0)
+		 }else{
+			 _czc.push(['_trackEvent', '规则页', 'btn', '轻启专属未同意'])
+			 alert('须阅读条款，并勾选同意')
+		 }
+		
+	})
+}
+
+function p3() {
+	p3UI.super(this);
+	var self = this;
+	var Event = laya.events.Event;
+
+	scaleRateW = (Laya.stage.width / Laya.stage.designWidth);//x缩放比例=舞台宽度/设计宽度
+	scaleRateH = (Laya.stage.height / Laya.stage.designHeight);//y缩放比例=舞台高度/设计高度
+	self.y=Laya.stage.height/2-Laya.stage.designHeight/2
+
+	if(Laya.stage.height<1500){
+		self.top1.y=self.top2.y=-self.y
+		self.logo.y=21-self.y
+		self.bottom1.y=1481+self.y
+		self.bottom2.y=1435+self.y
+	}
+
+	var przId=8
+	sf(1,self.top1,1.03,800)
+	sf(2,self.top2,1.03,800)
+	rotation(self.bottom1,-1,600)
+	rotation(self.bottom2,-2,600)
+
+	self.win.visible=false
+	self.losing.visible=false
+
+	butterfly(self.butterfly,0.8,0.95,0,0.8)
+	setTimeout(function() {
+		butterfly(self.butterfly2,0.8,0.95,0,0.8)
+	}, 300);
+
+	loop(self.flower1,2000,0)
+	loop(self.flower2,1600,0)
+	loop(self.flower3,2400,0)
+	function loop(obj,time,delay){
+		obj.rotation=0
+		Tween.to(obj,{rotation:360}, time, Ease.linearIn, Handler.create(self, function(){
+			loop(obj,time)
+		}), delay)
+	}
+	self.light.scale(0,0)
+	self.light.alpha=0
+	Tween.to(self.light,{scaleX:1,scaleY:1}, 1000, Ease.linearIn, Handler.create(self, function(){
+		_czc.push(['_trackEvent', '抽奖页', '礼盒动画', '抽奖'])
+		$.ajax({
+			type : 'post',
+			async : true,
+			url :  '//api.slb.moneplus.cn/api/web/yl/rand',
+			dataType: 'json',
+			data:{token:token,lng:longitude,lat:latitude},
+			success : function(res) {
+				console.log('rand',res);
+				if(res.code==0){
+					przId=res.data.prize_id
+					jump=res.data.prize_url
+					przId=='9'?losing():win()
+				}else{
+					alert(res.msg);
+				}
+			},
+			error : function(msg) {
+				alert(msg);
+			}
+		});
+		// setTimeout(function() {
+		// 	self.gift.visible=false
+		// 	Math.random()>0.5?win():losing()
+		// }, 500);
+	}), 10)
+	Tween.to(self.light,{alpha:0.7}, 600, Ease.linearIn, null, 10)
+
+	function win(){
+		self.win.visible=true
+		_czc.push(['_trackEvent', '结果页', '中奖', przId])
+
+		self.prize.skin='p3/prize'+przId+'.png'
+
+		butterfly(self.butterfly3,0.8,0.95,0,0.8)
+
+		// self.p3_t1.y=-200
+		// Tween.to(self.p3_t1,{y:400}, 800, Ease.backOut, null, 10)
+
+		// self.prize.x=Laya.stage.width
+		// Tween.to(self.prize,{x:2}, 700, Ease.backOut, null, 600)
+
+		self.p3_btn.scale(0,0)
+		Tween.to(self.p3_btn,{scaleX:1,scaleY:1}, 400, Ease.linearIn, Handler.create(self, function(){
+			sf(1,self.p3_btn,0.95,600)
+		}), 300)
+	}
+
+	self.p3_btn.once(Event.CLICK,this,function () {
+		// alert('(●′ω`●)领取成功')
+		_czc.push(['_trackEvent', 'btn', '领取卡包', przId])
+		setTimeout(function() {
+			window.location.replace(jump)
+			// window.location.href=jump
+		}, 500);
+	})
+
+	function losing(){
+		_czc.push(['_trackEvent', '结果页', '未中奖', '(●′ω`●)'])
+		self.losing.visible=true
+		butterfly(self.butterfly4,0.8,0.95,0,0.8)
+		loop(self.flower4,2000,0)
+		rotation(self.p4_leaves,-6,400)
+	}
+
 }
 
 var loadView = null;
@@ -98,19 +331,39 @@ function load() {
 	self.y=Laya.stage.height/2-Laya.stage.designHeight/2
 
 	console.log('load：'+Laya.stage.width+','+Laya.stage.height,scaleRateW,scaleRateH)
+	sf(1,self.load_left,1.03,600)
+	self.load_left.y=-self.y
 
-	butterfly(self.butterfly)
-	function butterfly(obj){
-		Tween.to(obj,{scaleX:0.9,scaleY:0.95,y:obj.y+2},650,Ease.linearIn, Handler.create(self, function(){
-			Tween.to(obj,{scaleX:1,scaleY:1,y:obj.y},650,Ease.linearIn, Handler.create(self, function(){
-				butterfly(obj)
-			}));		
-		}));
-	}
+	butterfly(self.butterfly,0.8,0.95,5,0.8)
+
+	wx.ready(function(){
+        // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+        wx.getLocation({
+			type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+			success: function (res) {
+				latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+				longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+				speed = res.speed; // 速度，以米/每秒计
+				accuracy = res.accuracy; // 位置精度
+				// latitude='39.916960'; // 纬度，浮点数，范围为90 ~ -90
+				// longitude='116.411040'; // 经度，浮点数，范围为180 ~ -180。
+				console.log('经纬度：',longitude,latitude)
+			},
+			fail: function (res){
+				console.log('fail',res)
+			},
+			complete: function (res){
+				console.log('complete',res)
+			}
+		});
+    });
+	
 }
 
 Laya.class(load, "load", loadUI);
 Laya.class(p1, "p1", p1UI);
+Laya.class(p2, "p2", p2UI);
+Laya.class(p3, "p3", p3UI);
 
 //程序入口
 getStageHeight()<1289?Laya.init(750,1500,Laya.WebGL):Laya.init(750,1500,Laya.WebGL)
@@ -120,7 +373,7 @@ os().pc?Laya.stage.scaleMode='showall':Laya.stage.scaleMode='fixedwidth'
 Laya.stage.alignH='center';
 Laya.stage.alignV='top';
 // 显示fps
-Laya.Stat.show(0,0);
+// Laya.Stat.show(0,0);
 // 背景颜色
 Laya.stage.bgColor='#fff';
 //背景透明
@@ -128,9 +381,15 @@ Laya.stage.bgColor='#fff';
 // Laya.Render.isWebGL ? Laya.stage.bgColor = "none" : Laya.stage.bgColor = null;
 
 var assets = [
-	// {url:'img/p1_bg.jpg',type:Laya.Loader.IMAGE},
+	{url:'img/rule1.png',type:Laya.Loader.IMAGE},
+	{url:'img/rule2.png',type:Laya.Loader.IMAGE},
+	{url:'img/rule3.png',type:Laya.Loader.IMAGE},
+	{url:'img/rule4.png',type:Laya.Loader.IMAGE},
+	{url:'img/p1_bg.jpg',type:Laya.Loader.IMAGE},
+	{url:'img/p3_bg.jpg',type:Laya.Loader.IMAGE},
 	{url:'res/atlas/p1.atlas',type:Laya.Loader.ATLAS},
 	{url:'res/atlas/p3.atlas',type:Laya.Loader.ATLAS},
+	{url:'res/atlas/p2.atlas',type:Laya.Loader.ATLAS},
 	{url:'res/atlas/p4.atlas',type:Laya.Loader.ATLAS},
 	{url:'res/atlas/gift.atlas',type:Laya.Loader.ATLAS},
 ];
@@ -158,8 +417,8 @@ function loading() {
 function onAssetLoaded(texture) {
 	console.log("加载结束",texture);
 	setTimeout(function() {
-		// Laya.stage.removeChild(loadView);
-		// Laya.stage.addChildAt(new p1(),zindex);
+		Laya.stage.removeChild(loadView);
+		Laya.stage.addChildAt(new p1(),zindex);
 	}, 500);
 }
 
--- src/ui/layaUI.max.all.js
+++ src/ui/layaUI.max.all.js
@@ -8,6 +8,7 @@ var loadUI=(function(_super){
 		    this.bar=null;
 		    this.percent=null;
 		    this.butterfly=null;
+		    this.load_left=null;
 
 			loadUI.__super.call(this);
 		}
@@ -21,12 +22,27 @@ var loadUI=(function(_super){
 
 		}
 
-		loadUI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"load/load_bg.png"}},{"type":"Image","props":{"y":1129,"x":176,"skin":"load/bar1.png"}},{"type":"Image","props":{"y":1129,"x":176,"width":400,"skin":"load/bar2.png","height":50},"child":[{"type":"Image","props":{"y":0,"x":-358,"var":"bar","skin":"load/bar2.png","renderType":"mask"}}]},{"type":"Label","props":{"y":1196,"x":337,"width":76,"var":"percent","text":"0%","styleSkin":"load/label.png","height":28,"fontSize":26,"color":"#ffd976","align":"center"}},{"type":"Image","props":{"y":1082,"x":578,"width":109,"var":"butterfly","skin":"load/butterfly.png","pivotY":71,"pivotX":73,"height":107}},{"type":"Image","props":{"y":0,"x":116,"skin":"load/load_box.png"}}]};
+		loadUI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"load/load_bg.png"}},{"type":"Image","props":{"y":1129,"x":176,"skin":"load/bar1.png"}},{"type":"Image","props":{"y":1129,"x":176,"width":400,"skin":"load/bar2.png","height":50},"child":[{"type":"Image","props":{"y":0,"x":-358,"var":"bar","skin":"load/bar2.png","renderType":"mask"}}]},{"type":"Label","props":{"y":1196,"x":337,"width":76,"var":"percent","text":"0%","styleSkin":"load/label.png","height":28,"fontSize":26,"color":"#ffd976","align":"center"}},{"type":"Image","props":{"y":0,"x":116,"skin":"load/load_box.png"}},{"type":"Image","props":{"y":1080,"x":580,"width":132,"var":"butterfly","skin":"load/butterfly.png","rotation":-34,"pivotY":41,"pivotX":66,"height":81}},{"type":"Image","props":{"y":0,"x":0,"var":"load_left","skin":"load/load_left.png"}}]};
 		return loadUI;
 	})(View);
 var p1UI=(function(_super){
 		function p1UI(){
 			
+		    this.top1=null;
+		    this.top2=null;
+		    this.bottom1=null;
+		    this.bottom2=null;
+		    this.logo=null;
+		    this.p1_t1=null;
+		    this.leaves=null;
+		    this.butterfly=null;
+		    this.p1_t2=null;
+		    this.prz1=null;
+		    this.prz2=null;
+		    this.prz3=null;
+		    this.prz4=null;
+		    this.gift=null;
+		    this.p1_t3=null;
 
 			p1UI.__super.call(this);
 		}
@@ -40,8 +56,80 @@ var p1UI=(function(_super){
 
 		}
 
-		p1UI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"p1/p1_bg.png"}}]};
+		p1UI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"img/p1_bg.jpg"}},{"type":"Image","props":{"y":0,"x":0,"var":"top1","skin":"p1/top1.png"}},{"type":"Image","props":{"y":0,"x":522,"var":"top2","skin":"p1/top2.png"}},{"type":"Image","props":{"y":1481,"x":27,"width":338,"var":"bottom1","skin":"p1/bottom1.png","pivotY":198,"pivotX":27,"height":217}},{"type":"Image","props":{"y":1435,"x":667,"width":183,"var":"bottom2","skin":"p1/bottom2.png","pivotY":80,"pivotX":100,"height":145}},{"type":"Image","props":{"y":71,"x":281,"var":"logo","skin":"p1/logo.png"}},{"type":"Image","props":{"y":235,"x":174,"var":"p1_t1","skin":"p1/p1_t1.png"}},{"type":"Image","props":{"y":387,"x":239,"width":70,"var":"leaves","skin":"p1/p1_leaves.png","pivotY":41,"pivotX":61,"height":54}},{"type":"Image","props":{"y":352,"x":591,"width":110,"var":"butterfly","skin":"load/butterfly.png","rotation":-27,"pivotY":34,"pivotX":55,"height":68}},{"type":"Image","props":{"y":434,"x":134,"var":"p1_t2","skin":"p1/p1_t2.png"}},{"type":"Image","props":{"y":548,"x":37,"var":"prz1","skin":"p1/prz1.png"}},{"type":"Image","props":{"y":548,"x":375,"var":"prz2","skin":"p1/prz2.png"}},{"type":"Image","props":{"y":836,"x":37,"var":"prz3","skin":"p1/prz3.png"}},{"type":"Image","props":{"y":836,"x":374,"var":"prz4","skin":"p1/prz4.png"}},{"type":"Image","props":{"y":1208,"x":368,"width":88,"var":"gift","skin":"p1/gift.png","pivotY":43,"pivotX":43,"height":84}},{"type":"Image","props":{"y":1280,"x":272,"var":"p1_t3","skin":"p1/p1_t3.png"}}]};
 		return p1UI;
+	})(View);
+var p2UI=(function(_super){
+		function p2UI(){
+			
+		    this.top1=null;
+		    this.top2=null;
+		    this.bottom1=null;
+		    this.bottom2=null;
+		    this.logo=null;
+		    this.p1_t1=null;
+		    this.leaves=null;
+		    this.butterfly=null;
+		    this.check=null;
+		    this.p2_btn=null;
+		    this.rule=null;
+
+			p2UI.__super.call(this);
+		}
+
+		CLASS$(p2UI,'ui.test.p2UI',_super);
+		var __proto__=p2UI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(p2UI.uiView);
+
+		}
+
+		p2UI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"img/p1_bg.jpg"}},{"type":"Image","props":{"y":0,"x":0,"var":"top1","skin":"p1/top1.png"}},{"type":"Image","props":{"y":0,"x":522,"var":"top2","skin":"p1/top2.png"}},{"type":"Image","props":{"y":1481,"x":27,"width":338,"var":"bottom1","skin":"p1/bottom1.png","pivotY":198,"pivotX":27,"height":217}},{"type":"Image","props":{"y":1435,"x":667,"width":183,"var":"bottom2","skin":"p1/bottom2.png","pivotY":80,"pivotX":100,"height":145}},{"type":"Image","props":{"y":71,"x":281,"var":"logo","skin":"p1/logo.png"}},{"type":"Image","props":{"y":213,"x":150,"var":"p1_t1","skin":"p1/p1_t1.png"}},{"type":"Image","props":{"y":365,"x":215,"width":70,"var":"leaves","skin":"p1/p1_leaves.png","pivotY":41,"pivotX":61,"height":54}},{"type":"Image","props":{"y":330,"x":567,"width":110,"var":"butterfly","skin":"load/butterfly.png","rotation":-27,"pivotY":34,"pivotX":55,"height":68}},{"type":"Image","props":{"y":1122,"x":125,"var":"check","skin":"p2/p2_check.png"}},{"type":"Image","props":{"y":1224,"x":200,"var":"p2_btn","skin":"p2/p2_btn.png"}},{"type":"Image","props":{"y":446,"x":97,"skin":"p2/rule_bg.png"}},{"type":"Panel","props":{"y":445,"x":92,"width":570,"var":"rule","vScrollBarSkin":"p2/vscroll.png","height":649},"child":[{"type":"Image","props":{"skin":"img/rule1.png"}},{"type":"Image","props":{"y":2000,"x":0,"skin":"img/rule2.png"}},{"type":"Image","props":{"y":4000,"x":0,"skin":"img/rule3.png"}},{"type":"Image","props":{"y":6000,"x":0,"skin":"img/rule4.png"}}]}]};
+		return p2UI;
+	})(View);
+var p3UI=(function(_super){
+		function p3UI(){
+			
+		    this.gift=null;
+		    this.butterfly=null;
+		    this.butterfly2=null;
+		    this.flower1=null;
+		    this.flower2=null;
+		    this.flower3=null;
+		    this.light=null;
+		    this.gift_box=null;
+		    this.win=null;
+		    this.butterfly3=null;
+		    this.p3_t1=null;
+		    this.prize=null;
+		    this.p3_btn=null;
+		    this.losing=null;
+		    this.butterfly4=null;
+		    this.flower4=null;
+		    this.p4_leaves=null;
+		    this.p4_t1=null;
+		    this.top1=null;
+		    this.top2=null;
+		    this.bottom1=null;
+		    this.bottom2=null;
+		    this.logo=null;
+
+			p3UI.__super.call(this);
+		}
+
+		CLASS$(p3UI,'ui.test.p3UI',_super);
+		var __proto__=p3UI.prototype;
+		__proto__.createChildren=function(){
+		    
+			laya.ui.Component.prototype.createChildren.call(this);
+			this.createView(p3UI.uiView);
+
+		}
+
+		p3UI.uiView={"type":"View","props":{"width":750,"height":1500},"child":[{"type":"Box","props":{"y":0,"x":0,"var":"gift"},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"img/p1_bg.jpg"}},{"type":"Image","props":{"y":543.1681958810595,"x":468,"width":132,"var":"butterfly","skin":"load/butterfly.png","rotation":40,"pivotY":41,"pivotX":66,"height":81}},{"type":"Image","props":{"y":730.1681958810595,"x":540,"width":166,"var":"butterfly2","skin":"load/butterfly.png","rotation":-28,"pivotY":51,"pivotX":83,"height":102}},{"type":"Image","props":{"y":640.1681958810595,"x":491,"width":40,"var":"flower1","skin":"gift/flower.png","pivotY":20,"pivotX":20,"height":40}},{"type":"Image","props":{"y":791.1681958810595,"x":299,"width":26,"var":"flower2","skin":"gift/flower.png","pivotY":13,"pivotX":13,"height":26}},{"type":"Image","props":{"y":770.1681958810595,"x":410,"width":34,"var":"flower3","skin":"gift/flower.png","pivotY":17,"pivotX":17,"height":34}},{"type":"Image","props":{"y":878.1681958810595,"x":377,"width":549,"var":"light","skin":"gift/light.png","pivotY":309,"pivotX":275,"height":327}},{"type":"Image","props":{"y":801.1681958810595,"x":249,"skin":"gift/gift_box.png"}},{"type":"Image","props":{"y":775.1681958810595,"x":192,"width":239,"var":"gift_box","skin":"gift/gift_box2.png","pivotY":190,"height":193}}]},{"type":"Box","props":{"y":0,"x":0,"var":"win"},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"img/p3_bg.jpg"}},{"type":"Image","props":{"y":373,"x":566,"width":110,"var":"butterfly3","skin":"load/butterfly.png","rotation":-27,"pivotY":34,"pivotX":55,"height":68}},{"type":"Image","props":{"y":400,"x":216,"var":"p3_t1","skin":"p3/p3_t1.png"}},{"type":"Image","props":{"y":581,"x":2,"var":"prize","skin":"p3/prize1.png"}},{"type":"Image","props":{"y":1063,"x":369,"width":319,"var":"p3_btn","skin":"p3/p3_btn.png","pivotY":42,"pivotX":160,"height":85}}]},{"type":"Box","props":{"y":0,"x":0,"var":"losing"},"child":[{"type":"Image","props":{"y":0,"x":0,"skin":"img/p3_bg.jpg"}},{"type":"Image","props":{"y":600,"x":578,"width":110,"var":"butterfly4","skin":"load/butterfly.png","rotation":-27,"pivotY":34,"pivotX":55,"height":68}},{"type":"Image","props":{"y":772,"x":125,"width":40,"var":"flower4","skin":"gift/flower.png","pivotY":20,"pivotX":20,"height":40}},{"type":"Image","props":{"y":923,"x":171,"width":182,"var":"p4_leaves","skin":"p4/p4_leaves.png","pivotY":171,"pivotX":125,"height":174}},{"type":"Image","props":{"y":650,"x":186,"var":"p4_t1","skin":"p4/p4_t1.png"}}]},{"type":"Image","props":{"y":0,"x":0,"var":"top1","skin":"p1/top1.png"}},{"type":"Image","props":{"y":0,"x":522,"var":"top2","skin":"p1/top2.png"}},{"type":"Image","props":{"y":1481,"x":27,"width":338,"var":"bottom1","skin":"p1/bottom1.png","pivotY":198,"pivotX":27,"height":217}},{"type":"Image","props":{"y":1435,"x":667,"width":183,"var":"bottom2","skin":"p1/bottom2.png","pivotY":80,"pivotX":100,"height":145}},{"type":"Image","props":{"y":71,"x":281,"var":"logo","skin":"p1/logo.png"}}]};
+		return p3UI;
 	})(View);
 var TestPageUI=(function(_super){
 		function TestPageUI(){
